5101505b22e7d20b542e42b3e15dfba1f9305ec2
JVLoe <108595341+JVLoe@users.noreply.github.com>
2023-10-09 16:33:18 +0100

    store blog posts as html files

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -8,7 +8,7 @@
     </head>
     <body>
         <article>
-            <h1><a href="/post">My First Post</a></h1>
+            <h1><a href="/posts/my-first-post">My First Post</a></h1>
 
             <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
@@ -26,7 +26,7 @@
         </article>
 
         <article>
-            <h1><a href="/post">My Second Post</a></h1>
+            <h1><a href="/posts/my-second-post">My Second Post</a></h1>
 
             <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
@@ -41,7 +41,7 @@
         </article>
 
         <article>
-            <h1><a href="/post">My Third Post</a></h1>
+            <h1><a href="/posts/my-third-post">My Third Post</a></h1>
 
             <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
