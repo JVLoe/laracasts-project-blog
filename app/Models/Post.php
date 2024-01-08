6717d185fd4c9a2bceddb048bcf8827a29c26fc2
JVLoe <108595341+JVLoe@users.noreply.github.com>
2024-01-08 19:15:24 +0000

    WIP: dynamic posts

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 class Post
 {
@@ -28,18 +29,19 @@ class Post
 
     public static function all()
     {
-        $files = File::files(resource_path("posts/"));
-
-        return array_map(fn($file) => $file->getContents(), $files);
+        return collect(File::files(resource_path("posts")))
+            ->map(fn($file) => YamlFrontMatter::parseFile($file))
+            ->map(fn($document) => new Post(
+                $document->title,
+                $document->excerpt,
+                $document->date,
+                $document->body(),
+                $document->slug
+            ));
     }
 
     public static function find($slug)
     {
-        base_path();
-        if (!file_exists($path = resource_path( "posts/{$slug}.html"))) {
-            throw new ModelNotFoundException();
-        }
-
-        return cache()->remember("posts.{$slug}", 3600, fn() => file_get_contents($path));
+        return static::all()->firstWhere('slug', $slug);
     }
 }
